554e079a966402c6e9f64a3dee94f21cc4b56ab3
teemupiiroinenwirepas <teemu.piiroinen@wirepas.com>
2021-09-07 13:01:18 +0300

    Update __init__.py

--- wirepas_networktool_messaging/__init__.py
+++ wirepas_networktool_messaging/__init__.py
@@ -21,31 +21,6 @@ from .__about__ import (
 
 from .proto import *
 
-#from .get_configs import GetConfigsRequest, GetConfigsResponse
-#from .get_gw_info import GetGatewayInfoRequest, GetGatewayInfoResponse
-#from .set_config import SetConfigRequest, SetConfigResponse
-#from .received_data import ReceivedDataEvent
-#from .status import StatusEvent, GatewayState
-#from .send_data import SendDataRequest, SendDataResponse
-#from .upload_scratchpad import UploadScratchpadRequest, UploadScratchpadResponse
-#from .process_scratchpad import ProcessScratchpadRequest, ProcessScratchpadResponse
-#from .get_scratchpad_status import GetScratchpadStatusRequest, GetScratchpadStatusResponse
-#from .set_scratchpad_target import SetScratchpadTargetAndActionRequest, SetScratchpadTargetAndActionResponse
-#from .gateway_result_code import GatewayResultCode
-#from .otap_helper import ScratchpadStatus, ScratchpadType, ScratchpadAction, ProcessingDelay
-#from .wirepas_exceptions import GatewayAPIParsingException
-#
-#from google.protobuf.internal import api_implementation
-
-# pylint: disable=locally-disabled, protected-access, wrong-import-order
-
-#try:
-#    if api_implementation._default_implementation_type == "python":
-#        print(__warning_msg__)
-#except AttributeError:
-#    print("Could not evaluate protobuf implementation type")
-
-
 __all__ = [
     "__author__",
     "__author_email__",
